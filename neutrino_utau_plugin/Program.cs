013c109eda14c8aa043fb904bf932ecadc7056f9
kokkiemouse <kokkiemouse@gmail.com>
2020-04-28 17:58:39 +0900

    ???

---
+++ neutrino_utau_plugin/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Win32;
+
+namespace neutrino_utau_plugin
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {   
+
+            if(args.Length < 1)
+            {
+                System.Console.WriteLine("ERROR ! You must set filename.");
+                return;
+            }
+            UST_MUSICXML_LIB.ustclass ustkun = new UST_MUSICXML_LIB.ustclass(args[0]);
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "MusicXML (*.musicxml)|*.musicxml|XML (*.xml)|*.xml|すべてのファイル (*.*)|*.*";
+            
+            if (saveFileDialog.ShowDialog()==true)
+            {
+                using(FileStream fskun=new FileStream(saveFileDialog.FileName, FileMode.Create))
+                {
+                    ustkun.Write_XML(fskun);
+                }
+            }
+            //デバッグ用
+            /*
+            using(MemoryStream memstr=new MemoryStream())
+            {
+                ustkun.Write_XML(memstr);
+                StreamReader sr = new StreamReader(memstr,Encoding.UTF8);
+                sr.BaseStream.Seek(0, SeekOrigin.Begin);
+                Console.WriteLine(sr.ReadToEnd());
+            }*/
+        }
+    }
+}
---
+++ neutrino_utau_plugin/README.md
@@ -0,0 +1,24 @@
+# NEUTRINO UTAU PLUGIN
+## このプラグインについて
+このプラグインはustをmusicxmlに変換するものです。
+## 使い方
+変換したいところを選択し、ツール → プラグイン -> Convert Music XML 
+と選択しましょう。
+
+するとファイル保存ダイアログが出てくるのでファイル名を入力して終了です.
+
+## 注意事項
+何が起きても責任は取らないゾ～
+
+このソフトは一応β版なので正式版に採用予定のNeutrinoを呼び出す機能は実装されていません。というかまだ作ってない( ;∀;) 
+
+## ライセンス
+GNU General Public License v3.0以降です。
+
+## 使用したライブラリ
+.net Framework 4.8
+
+[UST_MUSICXML_LIB (自作ライブラリ)](https://github.com/kokkiemouse/UST_MUSICXML_LIB)
+
+## 連絡先
+[Twitter](https://twitter.com/kokkiemouse2)
